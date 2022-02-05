da7a161d9abedbaf587c0380217dc7dd30edd8a2
koro <gotsteven645@gmail.com>
2022-02-05 21:16:15 +0100

    fixed random number generator... kinda

--- kahootgetter1.cpp
+++ kahootgetter1.cpp
@@ -1,12 +1,42 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+int secondtimes(std::string basic, std::string mUrl);
+std::string printout(std::string thisisacryforhelp, std::string mUrl);
 int main(){
+    //uh
+    int i, number;
+    time_t nTime;
+    srand((unsigned) time(&nTime));
+    //not even random tbh
+    int newest = rand() % 10 + 1;
+    std::string kCode = std::to_string(newest);
     std::string url = "https://kahoot.it/";
+    secondtimes(kCode, url);
+}
+int secondtimes(std::string basic, std::string mUrl){
+    for (int w = 0; w < 8; w++){
+            time_t nTime;
+            srand((unsigned) time(&nTime));
+            int sheeped = rand() % 10 ;
+            sheeped - 1;
+            std::string sheepedsheared = std::to_string(sheeped);
+            basic.append(sheepedsheared);
+        }
+        printout(basic, mUrl);
+}
+std::string printout(std::string thisisacryforhelp, std::string mUrl){
     for (int n = 0; n <= 10; n++){
-        int v1 = rand() % 999999999 + 111111111;
-        std::string validurl = url + std::to_string(v1);
-        std::string op = std::string("start ").append(validurl);
+        std::cout << thisisacryforhelp << std::endl;
+        if (thisisacryforhelp.length() >= 10){
+                main();
+        }
+        std::string realUrl = mUrl + thisisacryforhelp;
+        std::string op = std::string("start ").append(realUrl);
         system(op.c_str());
-        std::cout << op << std::endl;
+        main();
+        return op;
     }
     
+
 }
